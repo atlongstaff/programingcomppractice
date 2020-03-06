3569f3b5577498061c2fc8666b5ef3e2140d02c8
atlongstaff <42279859+atlongstaff@users.noreply.github.com>
2020-03-06 10:18:26 -0500

    2020-03-06
    made willowwildride work for every series in the file

--- LongstaffA2018-willowwildride.py
+++ LongstaffA2018-willowwildride.py
@@ -1,21 +1,25 @@
 data11 = open("DATA11.txt","r") 
 Totallist = data11.read()
 Totallist = Totallist.split("\n")
-TandN = Totallist[0]
-TandN = TandN.split(" ")
-T = int(TandN[0])
-N = int(TandN[1])
-boxage = 0
-newboxadded = False
-Totallist.pop(0)
-i=0
-while boxage > 0 or i < T or newboxadded == False:
-	if i <= T:
-		if Totallist[i] == "B":
-			boxage += T
-			newboxadded = True
-	if newboxadded == True:
-		boxage -=1	
-	i +=1	
+while len(Totallist) != 0:
+	TandN = Totallist[0]
+	TandN = TandN.split(" ")
+	T = int(TandN[0])
+	N = int(TandN[1])
+	boxage = 0
+	newboxadded = False
+	Totallist.pop(0)
+	iteration=0
+  
+	while boxage > 0 or iteration < T or newboxadded == False:
+		if iteration <= T:
+			if Totallist[iteration] == "B":
+				boxage += T
+				newboxadded = True
+		if newboxadded == True:
+			boxage -=1	
+		iteration +=1	
+	print(max(0,iteration - N))
+	for x in range(N):
+	    Totallist.pop(0)
 
-print(max(0,i - N))
